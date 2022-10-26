583e0cdaf8a1cd0d75ac5f4dbb852ef1d3a3d35a
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2022-10-26 15:15:49 +0200

    fixed contextmanager

--- tests/working_dir.py
+++ tests/working_dir.py
@@ -1,10 +1,12 @@
 import os
 from contextlib import contextmanager
 
+@contextmanager
 def working_dir(path: str):
   previous_dir = os.getcwd()
   try:
     os.chdir(path)
+    yield
   finally:
     os.chdir(previous_dir)
 
