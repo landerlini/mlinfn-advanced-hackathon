a6bdf596b7aefb3fe964e84eb213a7086a0382e4
Lucio Anderlini <Lucio.Anderlini@fi.infn.it>
2022-11-03 16:23:10 +0000

    fixes

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,7 +1,8 @@
+import pytest
 import os
 import nbformat
 from glob import glob
-from nbconvert.preprocessors import ExecutePreprocessor
+from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
 from working_dir import working_dir
 
 def __testdir (a_dir: str, file_pattern: str = "*.ipynb"):
@@ -25,8 +26,10 @@ def test_intro_gnn():
 def test_transformers():
   return __testdir('ex/gnn_transformers', 'TransformerSG.ipynb')
 
-def test_unet_train_and_predict():
-  __testdir('ex/unet', 'Train_UNET.ipynb')
+def test_unet_train_only():
+  return __testdir('ex/unet', 'Train_UNET.ipynb')
+
+def test_unet_predict_only():
   return __testdir('ex/unet', 'Predict_UNet.ipynb')
 
 def test_unet_intro():
@@ -36,7 +39,8 @@ def test_unet_generator():
   return __testdir('ex/unet/Data_Generator')
 
 def test_unet_arch():
-  return __testdir('ex/unet/UNet_Arch')
+    with pytest.raises(CellExecutionError):
+      return __testdir('ex/unet/UNet_Arch')
 
 def test_unet_loss():
   return __testdir('ex/unet/Loss_Metrics')
