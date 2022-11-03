3af461d00d57349c121329e454998f6983578656
Lucio Anderlini <Lucio.Anderlini@fi.infn.it>
2022-11-03 05:44:27 +0000

    added tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -24,3 +24,25 @@ def test_intro_gnn():
 
 def test_transformers():
   return __testdir('ex/gnn_transformers', 'TransformerSG.ipynb')
+
+def test_unet_train_and_predict():
+  __testdir('ex/unet', 'Train_UNet.ipynb')
+  return __testdir('ex/unet', 'Predict_UNet.ipynb')
+
+def test_unet_intro():
+  return __testdir('ex/unet', 'Lung_Segmentation_on_Chest_X-Ray_images_with_U-Net.ipynb')
+
+def test_unet_generator():
+  return __testdir('ex/unet/Data_Generator')
+
+def test_unet_arch():
+  return __testdir('ex/unet/UNet_Arch')
+
+def test_unet_loss():
+  return __testdir('ex/unet/Loss_Metrics')
+
+def test_DA_ML():
+  return __testdir('ex/domain_adaptation', 'Excercise_DA_MLhackathon.ipynb')
+
+def test_DA_ML_SimpleDNN():
+  return __testdir('ex/domain_adaptation', 'Excercise_DA_MLhackathon_SimpleDNN.ipynb')
